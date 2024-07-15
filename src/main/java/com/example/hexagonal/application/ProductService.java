ea83ffbc8304e79ac3bc8fc87d400e20d2abe3ce
Iván Palacios <ivanps@ext.inditex.com>
2024-07-15 14:42:29 +0200

    Commit inicial

---
+++ src/main/java/com/example/hexagonal/application/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.hexagonal.application;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductService {
+
+    public String getProductos() {
+        return "Listado de productos";
+    }
+}
